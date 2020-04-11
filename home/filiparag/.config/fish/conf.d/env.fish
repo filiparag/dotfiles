5d48f739c831ab7add1bdf0363aec170a9a2cd71
Filip Parag <filiparag.com@gmail.com>
2020-04-11 15:28:23 +0200

    sxhkd shell env var

--- home/filiparag/.config/fish/conf.d/env.fish
+++ home/filiparag/.config/fish/conf.d/env.fish
@@ -1,19 +1,22 @@
 # Configuration environment variables
 
 # Qt 5
-set -x QT_QPA_PLATFORMTHEME "qt5ct"
-set -x QT_AUTO_SCREEN_SCALE_FACTOR 0
+set -g -x QT_QPA_PLATFORMTHEME "qt5ct"
+set -g -x QT_AUTO_SCREEN_SCALE_FACTOR 0
 
 # Fzf
-set -x FZF_LEGACY_KEYBINDINGS 0
+set -g -x FZF_LEGACY_KEYBINDINGS 0
 
 # Go
-set -x GOPATH   $HOME/Projects/go
-set -x PATH     "$PATH:$GOPATH/bin"
+set -g -x GOPATH   $HOME/Projects/go
+set -g -x PATH     "$PATH:$GOPATH/bin"
 
 # Local binaries
-set -x PATH     "$PATH:$HOME/.local/bin"
+set -g -x PATH     "$PATH:$HOME/.local/bin"
 
 # JetBrains IDE
-set -x _JAVA_AWT_WM_NONREPARENTING 1
-set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true'
+set -g -x _JAVA_AWT_WM_NONREPARENTING 1
+set -g -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true'
+
+# Sxhkd
+set -g -x SXHKD_SHELL '/usr/bin/dash'
