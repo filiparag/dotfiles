5e36d1b4159856d3daa143a8fe36409773794c85
Filip Parag <filiparag.com@gmail.com>
2020-04-12 18:10:19 +0200

    fish abbrevations

--- home/filiparag/.config/fish/conf.d/mods.fish
+++ home/filiparag/.config/fish/conf.d/mods.fish
@@ -12,13 +12,31 @@ if status --is-interactive
 
     set -g fish_user_abbreviations
 
-    alias l            	'lsd --classify --almost-all --icon-theme unicode --icon always --color never --date --group-dirs first'
-    alias ll            'lsd --classify --long --almost-all --icon-theme unicode --icon always --blocks name,size,permission,date,user,group --color always --date --group-dirs first'
+    alias l            	'lsd --classify --almost-all --icon-theme unicode \
+                            --icon always --color never --date --group-dirs first'
+    alias ll            'lsd --classify --long --almost-all --icon-theme unicode \
+                            --icon always --blocks name,size,permission,date,user,group \
+                            --color always --date --group-dirs first'
+    
     alias subl          'subl3'
     alias dmenu         'rofi -dmenu'
 
+    abbr --add y        'yay'
     abbr --add ys       'yay -S'
     abbr --add yss      'yay -Ss'
     abbr --add yr       'yay -Rcsn'
+
+    abbr --add gs       'git status'
+    abbr --add ga       'git add'
+    abbr --add gc       'git commit -m'
+    abbr --add gp       'git push'
+    abbr --add gcl      'git clone'
+
+    abbr --add r        'cd /'
+    abbr --add h        'cd ~'
+
+    abbr --add v        'vim'
+    abbr --add c        'bat'
+    abbr --add s        'ssh'
 
 end
