07d797eadbc680ea9fcf88472852f054e76fc4ee
Filip Parag <filip@parag.rs>
2020-09-04 01:19:59 +0200

    group update

--- home/filiparag/.config/fish/conf.d/user.fish
+++ home/filiparag/.config/fish/conf.d/user.fish
@@ -3,5 +3,5 @@ set -x NAME     'Filip Parag'
 set -x EMAIL    'filip@parag.rs'
 set -x EDITOR   'vim'
 set -x BROWSER  'firefox'
-set -x VISUAL		'subl3'
+set -x VISUAL	'subl3 -w'
 set -x PATH     "$PATH:$HOME/.scripts"
