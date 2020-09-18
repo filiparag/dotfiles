50c3cea395a4aacd3ae41aa35e7565d280e26788
Filip Parag <filip@parag.rs>
2020-09-18 19:46:05 +0200

    readme; improved installation script

---
+++ README.md
@@ -0,0 +1,52 @@
+## Prerequisites
+
+To install these dotfiles using `archlinux.sh`, you should have:
+- base Arch Linux installed
+- at least 8 GiB of free space on root partition
+- user account with `sudo` privileges
+- access to the internet
+
+
+## Installation
+
+```bash
+# Clone dotfiles repository
+git clone https://github.com/filiparag/dotfiles.git /tmp/dotfiles
+
+# Run semi-automatic installer
+/tmp/dotfiles/archlinux.sh
+
+# Hardware-specific commands go here
+
+# Reboot your system to apply all updates
+sudo systemctl reboot
+```
+
+### Hardware-specific modifications
+
+If you are not using a newer AMD GPU:
+- install appropriate `xf86-video-` driver
+- make sure files in `/etc/X11/xorg.conf.d/` are compatible with you hardware
+
+
+## Customization
+
+User-specific environment variables are located in `~/.config/fish/conf.d/user.fish`.
+
+Window manager configuration is located in `~/.config/wmrc/rc.conf`.
+
+Startup applications are listed in `APPS` variable in `~/.config/wmrc/modules/services/apps`.
+
+Wallpaper and lockscreen are located in `~/Pictures` directory.
+
+To enable VNC server, run:
+``` bash
+# Set VNC password
+vncpasswd
+
+# Allow incoming VNC connections
+sudo ufw allow in 5900/tcp
+
+# Restart wmrc VNC module
+wmrc -r 'services/vnc(restart)'
+```
--- archlinux.sh
+++ archlinux.sh
@@ -2,64 +2,117 @@
 
 # Installation script for Arch Linux
 
-# Working and scratch directory
-DIR="$(dirname "$0")"
-TMPDIR="$(mktemp -d)"
+check_sudo() {
 
-# Install curl
-sudo pacman -Sy --needed curl
+	# Check if user can sudo
+	sudo -v
 
-# Generate new mirrorlist
-curl -L 'https://www.archlinux.org/mirrorlist/?country=AT&country=DE&country=HU&country=RO&country=RS&country=SI&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on' | sed 's|#Server|Server|' > "$TMPDIR/mirrorlist"
-sudo cp -f "$TMPDIR/mirrorlist" "/etc/pacman.d/mirrorlist"
+}
 
-# Update system
-sudo pacman -Syu
+workdir() {
 
-# Install yay
-mkdir -p "$TMPDIR/yay"
-cd "$TMPDIR/yay"
-curl -L 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay' > "$TMPDIR/yay/PKGBUILD"
-makepkg -si
+	# Working and scratch directory
+	DIR="$(realpath "$(dirname "$0")")" &&\
+	TMPDIR="$(mktemp -d)"
 
-# Install all required packages
-yay -S --needed - < "$DIR/pkglist"
+}
 
-# Use current username instead of 'filiparag'
-mv "$DIR/home/filiparag" "$DIR/home/$USER"
-rg --hidden -i filiparag -g '!.git' -g '!archlinux.sh' -l | xargs sed -i "s|filiparag|$USER|g"
+build_tools() {
 
-# Prepare dotfiles
-cp -rp "$DIR" "$HOME/.sydf"
-chown -R "$USER:$USER" "$HOME/.sydf"
-cd "$HOME/.sydf"
-git submodule update --init --recursive
-mkdir -p "$HOME/.config"
-echo "$HOME/.sydf" > "$HOME/.config/sydf.conf"
+	# Install build tools
+	sudo pacman -Sy --needed curl base-devel &&\
 
-# Download wallpaper
-mkdir -p "$HOME/.sydf/home/$USER/Pictures"
-curl -L 'http://ftp.parag.rs/wallpaper-day.png' > "$HOME/.sydf/home/$USER/Pictures/wallpaper-day.png"
-curl -L 'http://ftp.parag.rs/wallpaper-night.png' > "$HOME/.sydf/home/$USER/Pictures/wallpaper-night.png"
-cp -p "$HOME/.sydf/home/$USER/Pictures/wallpaper-night.png" "$HOME/.sydf/home/$USER/Pictures/lockscreen.png"
+	# Generate new mirrorlist
+	curl -L 'https://www.archlinux.org/mirrorlist/?country=AT&country=DE&country=HU&country=RO&country=RS&country=SI&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on' | sed 's|#Server|Server|' > "$TMPDIR/mirrorlist" &&\
+	sudo cp -f "$TMPDIR/mirrorlist" "/etc/pacman.d/mirrorlist" &&\
 
-# Replace provided mirrorlist with generated one
-cp -f "$TMPDIR/mirrorlist" "$DIR/etc/pacman.d/mirrorlist"
+	# Use all cores in makepkg.conf 
+	sudo sed 's/[ \t#]*MAKEFLAGS.*$/MAKEFLAGS="-j$(nproc)"/' -i /etc/makepkg.conf
+}
 
-# Hook dotfiles using sydf
-sydf hook
+install_packages() {
 
-# Install missing wmrc dependencies
-wmrc -m | yay -S --needed -
+	# Update system
+	sudo pacman -Syu &&\
 
-# Enable services
-sudo systemctl enable "sshd"
-sudo systemctl enable "cronie"
-sudo systemctl enable "NetworkManager"
-sudo systemctl enable "suspend@$USER"
-sudo systemctl enable "syncthing@$USER"
-sudo systemctl enable "syncthing-resume"
-sudo systemctl enable "systemd-resolved"
+	# Install yay
+	{
+		if ! command -v yay 1>/dev/null 2>/dev/null; then
+			mkdir -p "$TMPDIR/yay" &&\
+			cd "$TMPDIR/yay" &&\
+			curl -L 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay' > "$TMPDIR/yay/PKGBUILD" &&\
+			makepkg -si
+		fi
+	} &&\
 
-# Remove temporary files
-rm -rf "$TMPDIR"
+	# Install all required packages
+	yay -S --needed - < "$DIR/pkglist"
+
+}
+
+install_dotfiles() {
+
+	# Prepare dotfiles
+	cp -rp "$DIR" "$HOME/.sydf" &&\
+	sudo chown -R "$USER:$USER" "$HOME/.sydf" &&\
+	cd "$HOME/.sydf" &&\
+	git submodule update --init --recursive &&\
+	mkdir -p "$HOME/.config" &&\
+	echo "$HOME/.sydf" > "$HOME/.config/sydf.conf" &&\
+
+	# Use current username instead of 'filiparag'
+	{
+		if [ "$USER" != 'filiparag' ]; then
+			mv "$HOME/.sydf/home/filiparag" "$HOME/.sydf/home/$USER" &&\
+			rg --hidden -i filiparag -g '!.git' -g '!archlinux.sh' -l "$HOME/.sydf" | xargs sed -i "s|filiparag|$USER|g"
+		fi
+	} &&\
+
+	# Download wallpaper
+	mkdir -p "$HOME/.sydf/home/$USER/Pictures" &&\
+	curl -L 'http://ftp.parag.rs/wallpaper-day.png' > "$HOME/.sydf/home/$USER/Pictures/wallpaper-day.png" &&\
+	curl -L 'http://ftp.parag.rs/wallpaper-night.png' > "$HOME/.sydf/home/$USER/Pictures/wallpaper-night.png" &&\
+	cp -p "$HOME/.sydf/home/$USER/Pictures/wallpaper-night.png" "$HOME/.sydf/home/$USER/Pictures/lockscreen.png" &&\
+
+	# Replace provided mirrorlist with generated one
+	sudo cp -f "$TMPDIR/mirrorlist" "/etc/pacman.d/mirrorlist" &&\
+
+	# Hook dotfiles using sydf
+	sydf hook
+
+}
+
+wmrc_deps_and_services() {
+
+	# Install missing wmrc dependencies
+	wmrc -m | yay -S --needed - &&\
+
+	# Enable services
+	sudo systemctl enable "sshd" &&\
+	sudo systemctl enable "cronie" &&\
+	sudo systemctl enable "NetworkManager" &&\
+	sudo systemctl enable "suspend@$USER" &&\
+	sudo systemctl enable "syncthing@$USER" &&\
+	sudo systemctl enable "syncthing-resume" &&\
+	sudo systemctl enable "systemd-resolved" &&\
+	sudo systemctl enable "ufw" &&\
+
+	# Firewall
+	sudo ufw default deny incoming &&\
+	sudo ufw default allow outgoing &&\
+	sudo ufw allow ssh &&\
+	sudo ufw enable
+
+}
+
+finish_cleanup() {
+
+	# use fish as default shell
+	chsh -s /usr/bin/fish &&\
+
+	# Remove temporary files
+	rm -rf "$TMPDIR"
+
+}
+
+check_sudo && workdir && build_tools && install_packages && install_dotfiles && wmrc_deps_and_services && finish_cleanup
