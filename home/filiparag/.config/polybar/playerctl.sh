78cbfb8a044bd93b7b79daaca9a43192bf43e48f
Filip Parag <filip@parag.rs>
2020-10-13 00:49:33 +0200

    improve polybar playerctl module

--- home/filiparag/.config/polybar/playerctl.sh
+++ home/filiparag/.config/polybar/playerctl.sh
@@ -3,42 +3,25 @@
 output=""
 
 for p in $(playerctl -l 2>/dev/null); do
-	if [ $(playerctl -p $p status) = 'Playing' ] || [ $(playerctl -p $p status) = 'Paused' ]; then
-		track="$(
-			playerctl -p $p metadata | awk -v status="$(playerctl -p $p status)" \
-			'BEGIN {
-				artist=""
-				title=""
-			}
-			/xesam:artist/ {
-				if (NF>=3)
-					for (i=3;i<=NF;i++)
-						artist = artist sprintf("%s ",$i);
-			}
-			/xesam:title/ {
-				if (NF>=3)
-					for (i=3;i<=NF;i++)
-						title = title sprintf("%s ",$i);
-			}
-			END {
-				if (status == "Playing")
-					printf("󰎇 ")
-			else if (status == "Paused")
-					printf("󰎊 ")
-				if(length(artist) > 0)
-					printf("%s", artist)
-				if(length(artist) > 0 && length(title) > 0)
-					printf("– ")
-				if(length(title) > 0)
-					printf("%s", title)
-				printf("\n")
-
-			}'
+	status="$(playerctl -p "$p" status)"
+	if [ "$status" = 'Playing' ] || [ "$status" = 'Paused' ]; then
+		artist="$(
+			playerctl -p "$p" metadata -f '{{ xesam:artist }}' 
 		)"
+		title="$(
+			playerctl -p "$p" metadata -f '{{ xesam:title }}' | \
+			sed 's/([0-9]*) *//; s/ - YouTube$//'
+		)"
+		case "$(playerctl -p "$p" status)" in
+			Playing)
+				status_icon='󰝚';;
+			Paused)
+				status_icon='󰏤';;
+		esac
 		output="$(
-			printf "%s\n%s" "$output" "$track" | sed '/^$/d'
+			printf "%s\n%s %s%s" "$output" "$status_icon" "${artist:+$artist – }" "$title" | sed '/^$/d'
 		)"
 	fi
 done
 
-printf "%s" "$output" | LC_ALL=C sort -k1.1 | head -n 1 | cut -c 1-64
+printf "%s" "$output" | LC_ALL=C sort -r -k1.1 | head -n 1 | cut -c 1-64
