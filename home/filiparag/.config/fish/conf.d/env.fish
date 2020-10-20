b63479dd00b3f08a234683ed1d3a7c5e2f428518
Filip Parag <filip@parag.rs>
2020-10-20 21:22:46 +0200

    fix fish config, add gvfs dependency

--- home/filiparag/.config/fish/conf.d/env.fish
+++ home/filiparag/.config/fish/conf.d/env.fish
@@ -7,10 +7,6 @@ set -g -x QT_AUTO_SCREEN_SCALE_FACTOR 0
 # Fzf
 set -g -x FZF_LEGACY_KEYBINDINGS 0
 
-# Go
-set -g -x GOPATH   $HOME/Projects/go
-set -g -x PATH     "$PATH:$GOPATH/bin"
-
 # Local binaries
 set -g -x PATH     "$PATH:$HOME/.local/bin"
 
