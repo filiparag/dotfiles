8226d51f7fee5987a00bba1e5a5b062e9657aaae
Filip Parag <filip@parag.rs>
2021-07-03 19:02:30 +0200

    add auto-cpufreq; mpv youtube-dl resolution limit

--- src/HOME/.config/fish/conf.d/user.fish
+++ src/HOME/.config/fish/conf.d/user.fish
@@ -3,5 +3,6 @@ set -x NAME     'Filip Parag'
 set -x EMAIL    'filip@parag.rs'
 set -x EDITOR   'vim'
 set -x BROWSER  'firefox'
-set -x VISUAL	'subl -w'
+#set -x VISUAL	'subl -w'
+set -x VISUAL	'vim'
 set -x PATH     "$PATH:$HOME/.scripts"
