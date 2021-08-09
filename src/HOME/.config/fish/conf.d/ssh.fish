6cdae893516b0ea7c287c389c98fd87b2468d61f
Filip Parag <filip@parag.rs>
2021-08-09 22:45:06 +0200

    connect fish and keepassxc ssh-agent

--- src/HOME/.config/fish/conf.d/ssh.fish
+++ src/HOME/.config/fish/conf.d/ssh.fish
@@ -1,33 +1,51 @@
 set -x SSH_ENV $HOME/.cache/ssh_environment
+set -x SSH_KEEPASS_SOCK /run/user/(id -u)/keyring/ssh
+
+function ssh_use_keepassxc
+    if test -S $SSH_KEEPASS_SOCK &>/dev/null
+        if pgrep -xu filiparag keepassxc &>/dev/null
+            set -x SSH_AUTH_SOCK $SSH_KEEPASS_SOCK
+            return 0
+        else
+            return 2
+        end
+    else
+        return 1
+    end
+end
 
 function ssh_test_agent
-	ssh-add -l &> /dev/null
-	if [ $status = 2 ]
-		return 1
-	else
-		return 0
-	end
+    ssh-add -l &>/dev/null
+    if [ $status = 2 ]
+        return 1
+    else
+        return 0
+    end
 end
 
 function ssh_connect_agent
-	mkdir -p $HOME/.cache
-	touch $HOME/.cache/ssh_environment
-	. $SSH_ENV > /dev/null
-	ssh_test_agent
+    mkdir -p $HOME/.cache
+    touch $HOME/.cache/ssh_environment
+    . $SSH_ENV >/dev/null
+    ssh_test_agent
 end
 
 function ssh_start_agent
-	if not ssh_connect_agent
-		ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-      	chmod 600 $SSH_ENV 
-      	. $SSH_ENV > /dev/null
-	end
+    if not ssh_connect_agent
+        ssh-agent -c | sed 's/^echo/#echo/' >$SSH_ENV
+        chmod 600 $SSH_ENV
+        . $SSH_ENV >/dev/null
+    end
 end
 
 function ssh_stop_agent
-	if not ssh_connect_agent
-		return 1
-	else
-		kill $SSH_AGENT_PID
-	end
+    if not ssh_connect_agent
+        return 1
+    else
+        kill $SSH_AGENT_PID
+    end
+end
+
+function ssh_agent
+    ssh_use_keepassxc; or ssh_start_agent
 end
--- src/HOME/.config/fish/config.fish
+++ src/HOME/.config/fish/config.fish
@@ -1,9 +1,9 @@
 # Start SSH agent
-ssh_start_agent
+ssh_agent
 
 # Start X at login
 if status --is-login
-  if test -z "$DISPLAY" -a "$XDG_VTNR" -eq 1
-    exec startx -- -keeptty
-  end
+    if test -z "$DISPLAY" -a "$XDG_VTNR" -eq 1
+        exec startx -- -keeptty
+    end
 end
